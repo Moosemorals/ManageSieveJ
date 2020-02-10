72421f75afe28a3d4ffdc2aba0e41cd1501bdbbe
Osric Wilkinson <gitmoose@fluffypeople.com>
2020-02-10 01:14:34 +0000

    Improved unicde handling (tests)

--- src/test/java/com/fluffypeople/managesieve/ManageSieveClientTest.java
+++ src/test/java/com/fluffypeople/managesieve/ManageSieveClientTest.java
@@ -1,13 +1,47 @@
 package com.fluffypeople.managesieve;
 
 import org.testng.annotations.*;
+
+import java.io.*;
+
 import static org.testng.Assert.*;
 
 public class ManageSieveClientTest {
-   
+
     @Test
     public void test_isConnected() {
         ManageSieveClient client = new ManageSieveClient();
         assertFalse(client.isConnected(), "Shouldn't be connected");
+    }
+
+    @Test
+    public void test_ParseAsciiString() throws IOException, ParseException {
+        ManageSieveClient client = new ManageSieveClient();
+
+        String expected = "Hello World";
+        String encoded = "{" + expected.getBytes("UTF-8").length + "}\r\n" + expected;
+
+        StringReader in = new StringReader(encoded);
+        StringWriter out = new StringWriter();
+
+        client.setupForTesting(in, out);
+        String actual = client.parseString();
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void test_ParseUnicodeString() throws IOException, ParseException {
+        ManageSieveClient client = new ManageSieveClient();
+
+        String expected = "ÂÂβabc";
+        String encoded = "{" + expected.getBytes("UTF-8").length + "}\r\n" + expected;
+
+        StringReader in = new StringReader(encoded);
+        StringWriter out = new StringWriter();
+
+        client.setupForTesting(in, out);
+        String actual = client.parseString();
+
+        assertEquals(actual, expected);
     }
 }
